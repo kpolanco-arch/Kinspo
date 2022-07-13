bcb270063daeaa8271b6b31aa9b253f88e937839
karenopolanco <karenpalomino97@gmail.com>
2022-07-13 15:01:52 -0400

    changed base64

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,27 +36,23 @@ attr_reader :password
   end
 
   def password=(password)
-    @password = password
     self.password_digest = BCrypt::Password.create(password)
+    @password = password
+
   end
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
-
-  # def password=(password)
-  #       @password = password
-  #       self.password_digest = BCrypt::Password.create(password)
-  #   end
     
-    def ensure_session_token
-        self.session_token ||= SecureRandom.base64
-    end
+  def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+  end
 
-    def reset_session_token!
-        self.session_token = SecureRandom.base64
+  def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
         self.save!
         self.session_token
-    end
+  end
 
 end
